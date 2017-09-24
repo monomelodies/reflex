4ef50a7aeb2c6b55a36e60219ab1ae77cb45ff9f
Marijn Ophorst <marijn@monomelodies.nl>
2017-09-24 16:08:10 +0200

    wrap reflection type

---
+++ src/ReflectionType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Reflex;
+
+class ReflectionType extends \ReflectionType
+{
+    public function toNormalisedString() : string
+    {
+        if (is_callable($type)) {
+            return 'callable';
+        }
+        $type = gettype($type);
+        switch ($type) {
+            case 'integer': return 'int';
+            case 'boolean': return 'bool';
+            case 'double': return 'float';
+            default: return $type;
+        }
+    }
+}
+
