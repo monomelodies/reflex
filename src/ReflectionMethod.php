a11b59d9431b82a0a2c36f02c94a4508572f90ea
Marijn Ophorst <marijn@monomelodies.nl>
2017-09-24 17:05:20 +0200

    needs override to return wrapper

--- src/ReflectionMethod.php
+++ src/ReflectionMethod.php
@@ -5,5 +5,14 @@ namespace Monomelodies\Reflex;
 class ReflectionMethod extends \ReflectionMethod
 {
     use FunctionTrait;
+
+    public function getParameters() : array
+    {
+        $parameters = parent::getParameters();
+        array_walk($parameters, function (&$parameter) {
+            $parameter = new ReflectionParameter([$this->getDeclaringClass()->getName(), $this->getName()], $parameter->getName());
+        });
+        return $parameters;
+    }
 }
 
