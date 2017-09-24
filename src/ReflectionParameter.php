cdba242656078a753f8e9563e99e83cbb2dedb86
Marijn Ophorst <marijn@monomelodies.nl>
2017-09-24 16:08:04 +0200

    wrap reflection parameter

---
+++ src/ReflectionParameter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Reflex;
+
+class ReflectionParameter extends \ReflectionParameter
+{
+    public function getNormalisedType($param = null)
+    {
+        if (!isset($param)) {
+            $param = $this->getType();
+        }
+        if (is_object($param)) {
+            if ($compare = $this->getType()) {
+                $compare = $compare->__toString();
+                if ($oaram instanceof $compare) {
+                    return $compare;
+                }
+            }
+            return get_class($type);
+        }
+        return new ReflectionType($param)->toNormalisedString();
+    }
+
+    public function getType() : \ReflectionType
+    {
+        $type = parent::getType();
+        return new ReflectionType($type ? $type->__toString() : null);
+    }
+}
+
