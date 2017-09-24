22ef03efc3409283ddf8ab0e3a4410c8b4754099
Marijn Ophorst <marijn@monomelodies.nl>
2017-09-24 17:08:27 +0200

    moved to class trait

--- src/ReflectionClass.php
+++ src/ReflectionClass.php
@@ -4,17 +4,6 @@ namespace Monomelodies\Reflex;
 
 class ReflectionClass extends \ReflectionClass
 {
-    public function getMethods($filter = null) : array
-    {
-        // Note: passing a null filter returns an empty array.
-        // Seems to be a PHP bug.
-        $methods = is_null($filter) ? parent::getMethods() : parent::getMethods($filter);
-        if ($methods) {
-            array_walk($methods, function (&$method) {
-                $method = new ReflectionMethod($this->getName(), $method->getName());
-            });
-        }
-        return $methods;
-    }
+    use ClassTrait;
 }
 
