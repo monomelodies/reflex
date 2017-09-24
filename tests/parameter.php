8f7ae369c813786940a97b3bf82bf9423c011459
Marijn Ophorst <marijn@monomelodies.nl>
2017-09-24 16:43:47 +0200

    fix comment/test description

--- tests/parameter.php
+++ tests/parameter.php
@@ -4,7 +4,7 @@ return function () : Generator {
     /** Tests for Reflex's ReflectionParameter */
     yield function () : Generator {
         $reflection = new Monomelodies\Reflex\ReflectionParameter([Tests\Foo::class, 'bar'], 'string');
-        /** getMethods returns an array of wrapped reflection methds */
+        /** getType returns a wrapped instance of ReflectionType */
         yield function () use ($reflection) {
             $type = $reflection->getType();
             assert($type instanceof Monomelodies\Reflex\ReflectionType);
