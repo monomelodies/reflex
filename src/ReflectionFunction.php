3b89b9bb8f8706d6926788ef895508a9d864ffd1
Marijn Ophorst <marijn@monomelodies.nl>
2017-09-24 17:06:04 +0200

    also needs override

--- src/ReflectionFunction.php
+++ src/ReflectionFunction.php
@@ -5,5 +5,14 @@ namespace Monomelodies\Reflex;
 class ReflectionFunction extends \ReflectionFunction
 {
     use FunctionTrait;
+
+    public function getParameters() : array
+    {
+        $parameters = parent::getParameters();
+        array_walk($parameters, function (&$parameter) {
+            $parameter = new ReflectionParameter($this->getName(), $parameter->getName());
+        });
+        return $parameters;
+    }
 }
 
