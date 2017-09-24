ba62fac0c9deeff607eefc211e343e20fd4d9b2b
Marijn Ophorst <marijn@monomelodies.nl>
2017-09-24 16:06:46 +0200

    handle this better

--- src/ReflectionClass.php
+++ src/ReflectionClass.php
@@ -9,9 +9,11 @@ class ReflectionClass extends \ReflectionClass
         // Note: passing a null filter returns an empty array.
         // Seems to be a PHP bug.
         $methods = is_null($filter) ? parent::getMethods() : parent::getMethods($filter);
-        array_map($methods, function (&$methods) {
-            $method = new ReflectionFunction([$this->getName(), $method->getName()]);
-        });
+        if ($methods) {
+            array_walk($methods, function (&$method) {
+                $method = new ReflectionMethod($this->getName(), $method->getName());
+            });
+        }
         return $methods;
     }
 }
