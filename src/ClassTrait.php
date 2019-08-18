631d2fcd5b2538db5eab7649753426f8fd620391
Marijn Ophorst <marijn@monomelodies.nl>
2019-08-18 07:41:53 +0200

    this needs an override too

--- src/ClassTrait.php
+++ src/ClassTrait.php
@@ -16,5 +16,16 @@ trait ClassTrait
         }
         return $methods;
     }
+
+    public function getProperties(int $filter = null) : array
+    {
+        $properties = is_null($filter) ? parent::getMethods() : parent::getMethods($filter);
+        if ($properties) {
+            array_walk($properties, function (&$property) {
+                $property = new ReflectionProperty($this->getName(), $property->getName());
+            });
+        }
+        return $properties;
+    }
 }
 
