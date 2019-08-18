6c02e756a330341fb9b643c5b8229d7b39aa2d03
Marijn Ophorst <marijn@monomelodies.nl>
2019-08-18 07:41:46 +0200

    put this in a trait for reuse

--- src/ReflectionParameter.php
+++ src/ReflectionParameter.php
@@ -2,37 +2,9 @@
 
 namespace Monomelodies\Reflex;
 
-use Closure;
-
 class ReflectionParameter extends \ReflectionParameter
 {
     use Doccomment;
-
-    public function getNormalisedType($param = null) : string
-    {
-        if (!isset($param)) {
-            $param = $this->getType();
-        }
-        if (is_object($param) && !($param instanceof Closure)) {
-            if ($compare = $this->getType()) {
-                $compare = $compare->__toString();
-                if ($param instanceof $compare) {
-                    return $compare;
-                }
-            }
-            return get_class($param);
-        }
-        if (is_callable($param)) {
-            return 'callable';
-        }
-        $type = gettype($param);
-        switch ($type) {
-            case 'integer': return 'int';
-            case 'boolean': return 'bool';
-            case 'double': return 'float';
-            case 'Closure': return 'callable';
-            default: return $type;
-        }
-    }
+    use HasType;
 }
 
