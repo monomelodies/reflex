1e01714c9f667a5dabdb30afebc26e562eba998d
Marijn Ophorst <marijn@monomelodies.nl>
2019-08-18 07:29:44 +0200

    trait for getting cleaned doccomment

---
+++ src/Doccomment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Monomelodies\Reflex;
+
+trait Doccomment
+{
+    public function getCleanDocComment(bool $strip_annotations = true) : string
+    {
+        $doccomment = $this->getDocComment();
+        $doccomment = preg_replace("@^/\*\*@", '', $doccomment);
+        $doccomment = preg_replace("@\*/$@m", '', $doccomment);
+        if ($strip_annotations) {
+            $doccomment = preg_replace("/^\s*\*\s*@\w+.*?$/m", '', $doccomment);
+        }
+        $doccomment = preg_replace("@^\s*\*\s*@m", '', $doccomment);
+        $doccomment = str_replace("\n", ' ', $doccomment);
+        $doccomment = trim(preg_replace("@\s{2,}@", ' ', $doccomment));
+        return $doccomment;
+    }
+}
+
