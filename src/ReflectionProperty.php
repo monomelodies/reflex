150ea3b83fd0e26a4789bbedee93a1a43f2ebdb9
Marijn Ophorst <marijn@monomelodies.nl>
2020-02-09 13:16:36 +0100

    implement missing methods

--- src/ReflectionProperty.php
+++ src/ReflectionProperty.php
@@ -6,5 +6,33 @@ class ReflectionProperty extends \ReflectionProperty
 {
     use Doccomment;
     use HasType;
+
+    /**
+     * Get the file in which this property was declared.
+     */
+    public function getFileName() : string
+    {
+        return $this->getDeclaringClass()->getFileName();
+    }
+
+    /**
+     * Get the line where this property was declared.
+     */
+    public function getLine() : int
+    {
+        $code = file_get_contents($this->getFileName());
+        if ($this->isPrivate()) {
+            $modifier = 'private';
+        } elseif ($this->isProtected()) {
+            $modifier = 'protected';
+        } else {
+            $modifier = 'public';
+        }
+        if ($this->isStatic()) {
+            $modifier .= ' static';
+        }
+        $code = explode("$modifier $".$this->getName().";\n", $code);
+        return count(explode("\n", $code[0]));
+    }
 }
 
