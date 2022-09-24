b602d56ad24632de0d854e44c08566ebede11668
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-24 13:56:52 +0200

    locally store whatever function was passed, getName() will not work for closures

--- src/ReflectionFunction.php
+++ src/ReflectionFunction.php
@@ -2,16 +2,23 @@
 
 namespace Monomelodies\Reflex;
 
+use Closure;
+
 class ReflectionFunction extends \ReflectionFunction
 {
     use FunctionTrait;
     use Doccomment;
 
+    public function __construct(private Closure|string $function)
+    {
+        parent::__construct($function);
+    }
+
     public function getParameters() : array
     {
         $parameters = parent::getParameters();
         array_walk($parameters, function (&$parameter) {
-            $parameter = new ReflectionParameter($this->getName(), $parameter->getName());
+            $parameter = new ReflectionParameter($this->function, $parameter->getName());
         });
         return $parameters;
     }
