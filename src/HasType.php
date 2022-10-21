f2e4ea1638a12609fe1a8c62b94d403f2a89781c
Marijn Ophorst <marijn@monomelodies.nl>
2022-10-21 14:06:27 +0200

    let this account for union/intersection types

--- src/HasType.php
+++ src/HasType.php
@@ -2,7 +2,7 @@
 
 namespace Monomelodies\Reflex;
 
-use Closure;
+use ReflectionNamedParameter;
 
 trait HasType
 {
@@ -14,10 +14,17 @@ trait HasType
     public function getNormalisedType() :? string
     {
         if ($type = $this->getType()) {
-            return $type->getName();
-        } else {
-            return null;
+            if ($type instanceof ReflectionNamedType) {
+                return $type->getName();
+            }
+            if ($type instanceof ReflectionUnionType) {
+                return implode('|', $type->getTypes());
+            }
+            if ($type instanceof ReflectionIntersectionType) {
+                return implode('&', $type->getTypes());
+            }
         }
+        return null;
     }
 }
 
